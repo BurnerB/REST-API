c8964418532c9eafba5814af145a8210a9cf1c94
Barnaby <barnabywaweru@hotmail.com>
2019-04-16 19:26:44 +0300

    Data Structures

---
+++ Furniture store(Data Structures))/security.py
@@ -0,0 +1,20 @@
+from User import User
+
+users= [
+    User(1,"John","abcd")
+]
+
+username_mapping = {u.username:u for u in users}
+
+
+userid_mapping = {u.id:u for u in users}
+
+
+def authenticate(username,password):
+    user = username_mapping.get(username,None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    userid = payload["identity"]
+    return userid_mapping.get(userid,None)
